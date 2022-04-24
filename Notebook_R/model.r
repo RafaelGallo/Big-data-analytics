475a927e909b5a1826ee7dc858db78864deb0933
Rafael Gallo <rafaelhenriquegallo@gmail.com>
2022-04-23 21:00:11 -0300

    Add files via upload

---
+++ Notebook_R/model.r
@@ -0,0 +1,20 @@
+# Mini-Projeto 1 - Análise de Sentimentos em Redes Sociais
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+setwd("G:/Meu Drive/Machine learning e deep learning/Cursos - Google Drive/DSA/Formação cientista de dados 2.0/Big Data Analytics com R e Microsoft Azure Machine Learning/Projetos - mini projeto/Mini-Projeto 1 Análise de Redes Sociais")
+getwd()
+
+
+## Etapa 1 - Pacotes e Autenticação
+
+# Instalando e Carregando o Pacote twitteR
+install.packages("twitteR")
+install.packages("httr")
+library(twitteR)
+library(httr)
+
+
